9f26bc24f3a5b434c40061013c680c0f089507b1
Aimaadd <aimadbentayeb@gmail.Com>
2023-11-23 14:36:24 +0100

    checkvalues

---
+++ checkvalues.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checkvalues.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abentaye <abentaye@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 14:58:19 by abentaye          #+#    #+#             */
+/*   Updated: 2023/11/18 21:09:10 by abentaye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap_lib.h"
+
+void fill_stack(t_node stack, int argc, char **argv)
+{
+    if (argc == 2)
+        
+}
+
+int main(int argc, char **argv)
+{
+    
+}

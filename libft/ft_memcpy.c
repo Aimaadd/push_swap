be89619413dd210df493a403961aba00d56543fe
Aimaadd <abentaye@shi-r3-p5.s19.be>
2024-01-04 14:06:07 +0100

    libft in

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abentaye <abentaye@student.s19.be >        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/05 14:13:37 by abentaye          #+#    #+#             */
+/*   Updated: 2024/01/03 16:44:03 by abentaye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dst && !src)
+		return (NULL);
+	while (i < n)
+	{
+		((char *) dst)[i] = ((char *) src)[i];
+		i++;
+	}
+	return (dst);
+}
